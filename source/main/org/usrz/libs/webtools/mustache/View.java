7437c68fc3fd0541de9202818dc04faca516b162
Pier Fumagalli <pier@fumagalli.org>
2014-05-14 12:55:34 +0900

    View builder

--- source/main/org/usrz/libs/webtools/mustache/View.java
+++ source/main/org/usrz/libs/webtools/mustache/View.java
@@ -15,6 +15,13 @@
  * ========================================================================== */
 package org.usrz.libs.webtools.mustache;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
 import org.usrz.libs.utils.Check;
 
 public final class View {
@@ -23,12 +30,52 @@ public final class View {
     final Object scope;
 
     public View(String template) {
-        this(template, new Object[0]);
+        this(template, null);
     }
 
     public View(String template, Object scope) {
         this.template = Check.notNull(template, "Null template name");
-        this.scope = Check.notNull(scope, "Null scope");
+        this.scope = scope == null ? Collections.EMPTY_MAP : scope;
+    }
+
+    public static final Builder template(String template) {
+        return new Builder(template);
+    }
+
+    public static final class Builder {
+
+        private final String template;
+        private final Map<String, Object> scope = new HashMap<>();
+
+        private Builder(String template) {
+            this.template = Check.notNull(template, "Null template name");
+        }
+
+        public Builder with(String key, Object value) {
+            scope.put(key, value);
+            return this;
+        }
+
+        public View view() {
+            return new View(template, scope);
+        }
+
+        public ResponseBuilder response() {
+            return Response.ok(view());
+        }
+
+        public ResponseBuilder response(int status) {
+            return Response.status(status).entity(view());
+        }
+
+        public ResponseBuilder response(Response.Status status) {
+            return Response.status(status).entity(view());
+        }
+
+        public ResponseBuilder response(Response.StatusType status) {
+            return Response.status(status).entity(view());
+        }
+
     }
 
 }
