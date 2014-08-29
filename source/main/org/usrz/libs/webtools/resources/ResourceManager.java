1c66152c9b6ba53bc67a6dc23849218899737f42
Pier Fumagalli <ianosh@gmail.com>
2014-08-29 23:37:34 +0900

    What's up dawg?

--- source/main/org/usrz/libs/webtools/resources/ResourceManager.java
+++ source/main/org/usrz/libs/webtools/resources/ResourceManager.java
@@ -22,8 +22,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
+import org.usrz.libs.logging.Log;
+
 public class ResourceManager {
 
+    private final Log log = new Log();
     private final Charset charset;
     private final File root;
 
@@ -40,6 +43,7 @@ public class ResourceManager {
         } catch (IOException exception) {
             throw new ResourceException("I/O error checking root directory \"" + root + "\"", exception);
         }
+        log.debug("ResourceManager rooted at %s using default charset %s", this.root, this.charset.name());
     }
 
     public File getRootPath() {
